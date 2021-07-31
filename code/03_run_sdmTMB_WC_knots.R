edc2db774d07130a8159e1f6680d83ab98a8f2ce
Eric Ward <5046884+ericward-noaa@users.noreply.github.com>
2021-07-30 20:19:13 -0700

    Add files via upload

--- code/03_run_sdmTMB_WC_knots.R
+++ code/03_run_sdmTMB_WC_knots.R
@@ -1,6 +1,6 @@
 ## generalized paths; for running up to 1000 knots
 
-#devtools::install_github("pbs-assess/sdmTMB")
+#devtools::install_github("pbs-assess/sdmTMB", "priors-experimental")
 library(dplyr)
 library(sdmTMB)
 library(sp)
@@ -54,6 +54,36 @@ catch = dplyr::filter(catch, common_name == "sablefish")
 
 # Loop over species
 species = unique(catch$common_name)
+spp = 1
+
+haul_new = haul %>%
+  left_join(catch, by = "trawl_id") %>%
+  select(
+    trawl_id,
+    X,
+    Y,
+    latitude = latitude_dd.x,
+    longitude = longitude_dd.x,
+    year = year.x,
+    log_depth_scaled,
+    log_depth_scaled2,
+    cpue_kg_km2
+  )
+# Set NA CPUEs to 0
+haul_new$cpue_kg_km2[which(is.na(haul_new$cpue_kg_km2))] = 0
+
+# Assign folds / cross - validation
+set.seed(2021)
+n_folds = 8
+n_blocks = 8
+# first assign blocks
+haul_new$block = 1
+for(jj in 1:n_blocks) {
+  haul_new$block[which(haul_new$latitude < quantile(haul_new$latitude,1 - jj*(1/n_blocks)))] = jj+1
+}
+# now assign folds
+block_fold = data.frame("block"=1:n_blocks, "fold"=rep(1:n_folds,n_blocks/n_folds))
+haul_new = dplyr::left_join(haul_new, block_fold)
 
 # using AUC and Tweedie predictive density to evaluate performance
 # you can iterate fits over a range of number of knots by giving set of values rather than n_knots
@@ -61,68 +91,30 @@ performance <- data.frame(#knots = n_knots
   knots = seq(50, 1000, 50))
 
 for (k in 1:length(performance$knots)) {
-  haul_new = haul %>%
-    left_join(catch, by = "trawl_id") %>%
-    select(
-      trawl_id,
-      X,
-      Y,
-      latitude = latitude_dd.x,
-      longitude = longitude_dd.x,
-      year = year.x,
-      log_depth_scaled,
-      log_depth_scaled2,
-      cpue_kg_km2
-    )
-  # Set NA CPUEs to 0
-  haul_new$cpue_kg_km2[which(is.na(haul_new$cpue_kg_km2))] = 0
-  
+print(k)
   # Create species-specific directories to save output
   #if(!dir.exists(paste0("./output/WC/",species[spp]))) dir.create(paste0("./output/WC/",species[spp]))
+  spde = make_mesh(
+    haul_new,
+    xy_cols = c("X","Y"),
+    type = "kmeans",
+    n_knots = performance$knots[k],
+    #cutoff=70,
+    seed = 2021
+  )
   
   if (use_cv == TRUE) {
     # create ids based on latitude quantiles
-    set.seed(2021)
-    n_folds = 5
-    n_blocks = 20
-    # first assign blocks
-    haul_new$block = 1
-    for (jj in 1:n_blocks) {
-      haul_new$block[which(haul_new$latitude < quantile(haul_new$latitude, 1 - jj *
-                                                          (1 / n_blocks)))] = jj + 1
-    }
-    # now assign folds
-    block_fold = data.frame("block" = 1:n_blocks,
-                            "fold" = rep(1:n_folds, n_blocks / n_folds))
-    haul_new = dplyr::left_join(haul_new, block_fold)
-    
-    spde = make_mesh(
-      haul_new,
-      n_knots = performance$knots[k],
-      xy_cols = c("X", "Y"),
-      type = "kmeans",
-      seed = 2021
-    )
-    
     tic() # start calculation of runtime
     
-    density_model <- sdmTMB_cv(
-      cpue_kg_km2 ~ 0 + as.factor(year),
-      time_varying = ~ 0 + log_depth_scaled + log_depth_scaled2,
-      spde = spde,
-      #k_folds = max(haul_new$fold),
-      #fold_ids = haul_new$fold,
-      k_folds = 5,
-      data = haul_new,
-      time = "year",
-      priors = sdmTMBpriors(
-        matern_s = pc_matern(range_gt = 5, sigma_lt = 5),
-        matern_st = pc_matern(range_gt = 5, sigma_lt = 5)
-      ),
-      anisotropy = TRUE,
-      spatial_only = TRUE,
-      family = tweedie(link = "log")
-    )
+    density_model <- sdmTMB_cv(cpue_kg_km2 ~ 0 + as.factor(year)+ log_depth_scaled + log_depth_scaled2,
+                               spde = spde,
+                               k_folds = max(haul_new$fold),
+                               fold_ids = haul_new$fold,
+                               data = haul_new,
+                               priors = sdmTMBpriors(matern_s = pc_matern(range_gt = 5, sigma_lt = 5)),
+                               spatial_only = TRUE,
+                               family = tweedie(link = "log"))
     
     saveRDS(
       density_model,
