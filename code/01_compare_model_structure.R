a5e48df33b77e5c4a2e6f6e587333c4e5409803e
Lewis Barnett <labarnett@ucdavis.edu>
2021-08-30 17:20:48 -0700

    expand quantile model code to include all cases

--- code/01_compare_model_structure.R
+++ code/01_compare_model_structure.R
@@ -85,37 +85,64 @@ saveRDS(model_1_full, "results/fit_spatial_depth_full.rds")
 
 ################################################################################
 
-# Spatial Extent of Data:
-
-library(sdmTMB)
-
-# full dataset
-haul_new = readRDS("data/sablefish_cleaned.rds")
+# Fit models after filtering spatial extent of data:
 
 # data filtering based on latitude quantiles
-#haul_new = dplyr::filter(haul_new, latitude <= quantile(latitude, 0.975),
-#                         latitude >= quantile(latitude, 0.025)) # 95% quantile
+haul_95 = dplyr::filter(haul_new, latitude <= quantile(latitude, 0.975),
+                         latitude >= quantile(latitude, 0.025)) # 95% quantile
 
-#haul_new = dplyr::filter(haul_new, latitude <= quantile(latitude, 0.95),
-#                         latitude >= quantile(latitude, 0.05)) # 90% quantile
+haul_90 = dplyr::filter(haul_new, latitude <= quantile(latitude, 0.95),
+                         latitude >= quantile(latitude, 0.05)) # 90% quantile
 
-haul_new = dplyr::filter(haul_new, latitude <= quantile(latitude, 0.90),
+haul_80 = dplyr::filter(haul_new, latitude <= quantile(latitude, 0.90),
                          latitude >= quantile(latitude, 0.1)) # 80% quantile
 
-# make new mesh
-spde = make_mesh(
-  haul_new,
+# make new meshes
+spde_95 = make_mesh(
+  haul_95,
+  xy_cols = c("X","Y"),
+  type = "cutoff",
+  cutoff = 15,
+  seed = 2021
+)
+spde_90 = make_mesh(
+  haul_90,
+  xy_cols = c("X","Y"),
+  type = "cutoff",
+  cutoff = 15,
+  seed = 2021
+)
+spde_80 = make_mesh(
+  haul_80,
   xy_cols = c("X","Y"),
   type = "cutoff",
   cutoff = 15,
   seed = 2021
 )
 
-model_1_full_80quantile <- sdmTMB(cpue_kg_km2 ~ 0 + as.factor(year) + log_depth_scaled + log_depth_scaled2,
-                       spde = spde,
-                       data = haul_new,
+model_1_full_95quantile <- sdmTMB(cpue_kg_km2 ~ 0 + as.factor(year) + log_depth_scaled + log_depth_scaled2,
+                       spde = spde_95,
+                       data = haul_95,
                        time = "year",
                        priors = sdmTMBpriors(matern_s = pc_matern(range_gt = 75, sigma_lt = 5)),
                        spatial_only = TRUE,
                        family = tweedie(link = "log"))
+saveRDS(model_1_full_95quantile, "results/fit_spatial_depth_full_95quantile.rds")
+
+model_1_full_90quantile <- sdmTMB(cpue_kg_km2 ~ 0 + as.factor(year) + log_depth_scaled + log_depth_scaled2,
+                                  spde = spde_90,
+                                  data = haul_90,
+                                  time = "year",
+                                  priors = sdmTMBpriors(matern_s = pc_matern(range_gt = 75, sigma_lt = 5)),
+                                  spatial_only = TRUE,
+                                  family = tweedie(link = "log"))
+saveRDS(model_1_full_90quantile, "results/fit_spatial_depth_full_90quantile.rds")
+
+model_1_full_80quantile <- sdmTMB(cpue_kg_km2 ~ 0 + as.factor(year) + log_depth_scaled + log_depth_scaled2,
+                                  spde = spde_80,
+                                  data = haul_80,
+                                  time = "year",
+                                  priors = sdmTMBpriors(matern_s = pc_matern(range_gt = 75, sigma_lt = 5)),
+                                  spatial_only = TRUE,
+                                  family = tweedie(link = "log"))
 saveRDS(model_1_full_80quantile, "results/fit_spatial_depth_full_80quantile.rds")
