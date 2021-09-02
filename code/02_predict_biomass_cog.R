b990e36a5d6e9dbb6a4b081bd4b0d9bc921ffef1
Lewis Barnett <labarnett@ucdavis.edu>
2021-09-02 14:52:45 -0700

    clean up and rename

--- code/02_predict_biomass_cog.R
+++ code/02_predict_biomass_cog.R
@@ -5,21 +5,20 @@ library(sdmTMB)
 # select years to predict and gather replicate prediction data for each of these years
 years = 2003:2018
 wc_grid = readRDS("data/wc_grid_1x.rds")
-#wc_grid = readRDS("data/wc_grid_4x.rds")
-Predict_data_years = wc_grid
-Predict_data_years$year = 2003
+wc_grid_predict = wc_grid
+wc_grid_predict$year = 2003
 
 for(i in 2:length(unique(years))) {
   wc_grid$year = unique(years)[i]
-  Predict_data_years = rbind(Predict_data_years, wc_grid)
+  wc_grid_predict = rbind(wc_grid_predict, wc_grid)
 }
 rm(wc_grid)
 
 # load fitted model
 m = readRDS("results/fit_spatial_depth_full.rds")
 
-# predict to grid to estimate COG, biomass index with 95% CI
-p = predict(m, newdata = Predict_data_years, return_tmb_object = TRUE)
+# predict to grid to estimate COG, biomass index with CI (default 95%)
+p = predict(m, newdata = wc_grid_predict, return_tmb_object = TRUE)
 saveRDS(p, "results/predictions_1x.rds")
 
 # compute indices (takes a lot of memory for 1x resolution predictions)
@@ -30,7 +29,36 @@ gc()
 
 COG = get_cog(p, bias_correct = TRUE)
 saveRDS(COG, "results/COG_1x_bias_corrected.rds")
-rm(COG, m, p)
+rm(COG, p)
+gc()
+
+################################################################################
+
+# repeat above computations at 4x the spatial resolution of prediction grid
+
+wc_grid_4x = readRDS("data/wc_grid_4x.rds")
+wc_grid_4x_predict = wc_grid_4x
+wc_grid_4x_predict$year = 2003
+
+for(i in 2:length(unique(years))) {
+  wc_grid_4x$year = unique(years)[i]
+  wc_grid_4x_predict = rbind(wc_grid_4x_predict, wc_grid_4x)
+}
+rm(wc_grid_4x)
+
+# predict to grid to estimate COG, biomass index with 95% CI
+p_4x = predict(m, newdata = wc_grid_4x_predict, return_tmb_object = TRUE)
+saveRDS(p_4x, "results/predictions_4x.rds")
+
+# compute indices (takes a lot of memory for 1x resolution predictions)
+biomass_estimates_4x = get_index(p_4x, bias_correct = TRUE) # bias correction on
+saveRDS(biomass_estimates_4x, "results/biomass_4x_bias_corrected.rds")
+rm(biomass_estimates_4x)
+gc()
+
+COG_4x = get_cog(p_4x, bias_correct = TRUE)
+saveRDS(COG_4x, "results/COG_4x_bias_corrected.rds")
+rm(COG_4x, p_4x)
 gc()
 
 ################################################################################
@@ -39,7 +67,7 @@ gc()
 
 m_95 = readRDS("results/fit_spatial_depth_full_95quantile.rds")
 
-p_95 = predict(m_95, newdata = Predict_data_years, return_tmb_object = TRUE)
+p_95 = predict(m_95, newdata = wc_grid_predict, return_tmb_object = TRUE)
 saveRDS(p_95, "results/predictions_1x_95quantile.rds")
 
 biomass_estimates_95 = get_index(p_95, bias_correct = TRUE) # bias correction on
@@ -55,7 +83,7 @@ gc()
 
 m_90 = readRDS("results/fit_spatial_depth_full_90quantile.rds")
 
-p_90 = predict(m_90, newdata = Predict_data_years, return_tmb_object = TRUE)
+p_90 = predict(m_90, newdata = wc_grid_predict, return_tmb_object = TRUE)
 saveRDS(p_90, "results/predictions_1x_90quantile.rds")
 
 biomass_estimates_90 = get_index(p_90, bias_correct = TRUE) # bias correction on
@@ -71,7 +99,7 @@ gc()
 
 m_80 = readRDS("results/fit_spatial_depth_full_80quantile.rds")
 
-p_80 = predict(m_80, newdata = Predict_data_years, return_tmb_object = TRUE)
+p_80 = predict(m_80, newdata = wc_grid_predict, return_tmb_object = TRUE)
 saveRDS(p_80, "results/predictions_1x_80quantile.rds")
 
 biomass_estimates_80 = get_index(p_80, bias_correct = TRUE) # bias correction on
