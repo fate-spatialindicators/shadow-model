24b4af6046ecb509c7ae82cb3cfcb1a8bcdd105e
Lewis Barnett <labarnett@ucdavis.edu>
2021-09-02 17:53:28 -0700

    biomass comparison

--- code/04_plot_biomass_cog.R
+++ code/04_plot_biomass_cog.R
@@ -12,15 +12,17 @@ cog4 <- readRDS("~/best-practices/results/COG_4x_bias_corrected.rds")
 b1 <- readRDS("~/best-practices/results/biomass_1x_bias_corrected.rds")
 b4 <- readRDS("~/best-practices/results/biomass_4x_bias_corrected.rds")
 
+# plot COGs ----
+
 # set up error bars for plotting in two dimensions, where COG is not dynamic in spatial-only model
 cog1 <- cog1 %>%
   distinct(coord, .keep_all = TRUE) %>%
   select(est, lwr, upr, coord) %>%
-  mutate(res = "fine")
+  mutate(res = "fine (1x)")
 cog4 <- cog4 %>%
   distinct(coord, .keep_all = TRUE) %>%
   select(est, lwr, upr, coord) %>%
-  mutate(res = "coarse")
+  mutate(res = "coarse (4x)")
 cogs <- bind_rows(cog1, cog4)
 
 cogs_wide_est <- reshape2::dcast(cogs, res ~ coord, value.var = "est")
@@ -44,3 +46,52 @@ ggplot(cogs_wide, aes(X, Y, color = factor(res))) +
   labs(color = "resolution") +
   theme_classic()
 ggsave("plots/cog_resolution_comparison.pdf", width = 4.5, height = 4, units = "in")
+
+
+# plot biomass over time ----
+
+# make estimates relative to max est
+b1$est_rel = b1$est/max(b1$est)
+b1$lwr_rel = b1$lwr/max(b1$est)
+b1$upr_rel = b1$upr/max(b1$est)
+b4$est_rel = b4$est/max(b4$est)
+b4$lwr_rel = b4$lwr/max(b4$est)
+b4$upr_rel = b4$upr/max(b4$est)
+
+# add column for grid resolution
+b1$res <- "Fine (1x)"
+b4$res <- "Coarse (4x)"
+b = bind_rows(b1, b4)
+
+# plot time series (means are identical for this model)
+ggplot(b, aes(x=year, y=est_rel, color=factor(res)), group=res) +
+  geom_point(size=3) +
+  geom_line() +
+  geom_errorbar(aes(x=year,ymin=lwr_rel, ymax=upr_rel), width=0) +
+  scale_x_continuous(breaks = seq(from = 2003, to = 2018, by = 1)) +
+  scale_color_viridis(discrete = TRUE, begin = 0, end = 0.8, breaks=c("Fine (1x)","Coarse (4x)")) +
+  xlab("Year") +
+  ylab("Relative Biomass Estimate") +
+  labs(color = "resolution") +
+  theme_classic()
+
+# Coefficient of Variation (CV) across years
+b1_mean = mean(b1$est)
+b1_sd = sd(b1$est)
+b1_cv = b1_sd/b1_mean
+
+b4_mean = mean(b4$est)
+b4_sd = sd(b4$est)
+b4_cv = b4_sd/b4_mean
+
+# CVs for each year (Annual CVs)
+b1_cvs = data.frame("year" = 2003:2018)
+b1_cvs[, "CV"] = b1$se/b1$log_est
+mean(b1_cvs$CV)
+
+b4_cvs = data.frame("year" = 2003:2018)
+b4_cvs[, "CV"] = b4$se/b4$log_est
+mean(b4_cvs$CV)
+
+
+
